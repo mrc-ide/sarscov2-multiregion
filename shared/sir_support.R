77dfebd7802e1e7129ec84991652765c396280b2
edknock <e.knock@imperial.ac.uk>
2024-06-19 12:21:16 +0100

    add sir_support.R

---
+++ shared/sir_support.R
@@ -0,0 +1,8 @@
+transform_pars <- function (p) {
+  list(alpha = p[["alpha"]],
+       beta = p[["beta"]],
+       gamma = p[["gamma"]],
+       rho = p[["rho"]],
+       N = 10000,
+       I0 = 10)
+}
