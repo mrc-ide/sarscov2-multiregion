8b29da773993479a7170d7549a7c94059e1c0454
edknock <e.knock@imperial.ac.uk>
2024-10-09 16:44:25 +0100

    rename orderly.R and used named arguments in orderly_artefact

---
+++ src/sarscov2_data/sarscov2_data.R
@@ -0,0 +1,18 @@
+orderly2::orderly_resource(
+  c("outputs/data_vaccination.csv",
+    "outputs/england_region_data.csv",
+    "outputs/hfr_week.csv",
+    "outputs/outcomes_vacc_status.csv",
+    "outputs/serology_for_inference.csv",
+    "outputs/severity_data.csv",
+    "outputs/weighted_prior_ranges.csv"))
+
+orderly2::orderly_artefact(
+  description = "Data for use in fits",
+  files = c("outputs/data_vaccination.csv",
+            "outputs/england_region_data.csv",
+            "outputs/hfr_week.csv",
+            "outputs/outcomes_vacc_status.csv",
+            "outputs/serology_for_inference.csv",
+            "outputs/severity_data.csv",
+            "outputs/weighted_prior_ranges.csv"))
